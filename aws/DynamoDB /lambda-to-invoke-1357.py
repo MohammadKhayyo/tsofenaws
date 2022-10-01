737c6e8fc901e2019819ef31cb4c19cc5a306340
MohammadKhayyo <mihamdkh@post.jce.ac.il>
2022-10-01 00:34:25 -0400

    fix size

--- aws/DynamoDB /lambda-to-invoke-1357.py
+++ aws/DynamoDB /lambda-to-invoke-1357.py
@@ -4,21 +4,19 @@ import uuid
 import hashlib
 s3 = boto3.client('s3')
 client = boto3.client('dynamodb')
-
-
 def lambda_handler(event, context):
     try:
-        bucket_invoker = event["bucket"]
-        key_invoker = event["key"]
+        bucket_invoker=event["bucket"]
+        key_invoker=event["key"]
         response = s3.get_object(Bucket=bucket_invoker, Key=key_invoker)
-        text_bytes = response["Body"].read()
+        text_bytes=response["Body"].read()
         readable_hash = hashlib.sha256(text_bytes).hexdigest()
         data = client.scan(TableName='DynamoDB-to-invoke')
         for item in data['Items']:
             if readable_hash == item['hashlib-sha256-for-content']['S']:
                 occurrence = 2
-                if 'occurrence' in item.keys():
-                    occurrence = int(item['occurrence']['N']) + 1
+                if 'occurrence'in item.keys():
+                    occurrence=int(item['occurrence']['N']) + 1
                 new_item = {
                     'hashlib-sha256-for-content': {
                         'S': item['hashlib-sha256-for-content']['S'],
@@ -35,22 +33,22 @@ def lambda_handler(event, context):
                     'list of words of the first line': {
                         'SS': item['list of words of the first line']['SS']
                     },
-                    'occurrence': {
-                        'N': f'{occurrence}'
+                    'occurrence' : {
+                        'N' : f'{occurrence}'
                     }
                 }
                 response = client.put_item(
                     TableName='DynamoDB-to-invoke',
-                    Item=new_item,
+                    Item = new_item,
                     ReturnConsumedCapacity='TOTAL'
                 )
-                return json.dumps({'status': 200, 'message': 'Success!'})
-        url = f'https://{bucket_invoker}.s3.amazonaws.com/{key_invoker}'
-        name = key_invoker
-        size = response["ContentLength"]
-        text_string = text_bytes.decode('ASCII')
+                return json.dumps({'status' : 200,'message' : 'Success!'})
+        url=f'https://{bucket_invoker}.s3.amazonaws.com/{key_invoker}'
+        name=key_invoker
+        text_string=text_bytes.decode('ASCII')
+        size=len(text_string)
         first_line = text_string.split('\n', 1)[0]
-        list_of_words_of_the_first_line = first_line.split()
+        list_of_words_of_the_first_line=first_line.split()
         new_item = {
             'hashlib-sha256-for-content': {
                 'S': readable_hash,
@@ -70,12 +68,11 @@ def lambda_handler(event, context):
         }
         response = client.put_item(
             TableName='DynamoDB-to-invoke',
-            Item=new_item,
+            Item = new_item,
             ReturnConsumedCapacity='TOTAL'
         )
-        s3.put_object(Body=text_string,
-                      Bucket='bucket-lambda-to-invoke-1357', Key=key_invoker)
-        return json.dumps({'status': 200, 'message': 'Success!'})
+        s3.put_object(Body=text_string, Bucket='bucket-lambda-to-invoke-1357', Key=key_invoker)
+        return json.dumps({'status' : 200,'message' : 'Success!'})
     except Exception as e:
-        print("e", e)
-        return json.dumps({"status": 500, "message": e})
+        print("e",e)
+        return json.dumps({"status" : 500,"message" : e})
